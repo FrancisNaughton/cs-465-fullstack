50a6b5aeb79059c90917073bbc7489602fb6379f
FrancisNaughton <francis.naughton301@gmail.com>
2023-05-11 13:01:16 -0400

    Static HTML to Templates with JSON

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -5,7 +5,7 @@ var trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'));
 const travel = (req, res) => {
     pageTitle = process.env.npm_package_description + ' - Travel';
     res.render('travel', {title: pageTitle, trips});
-}; 
+};
 
 module.exports = {
     travel
